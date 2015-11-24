fe59ae40dbb88054178e911c702c6d2dc4da2c0f
Guest User <Guest@stk-cs01-ws05.westlothian.sch.uk>
2015-11-24 10:30:47 +0000

    2.1
    Equivalent to 2.3 on sprint example

--- TOPPUPIL.java
+++ TOPPUPIL.java
@@ -1,15 +1,17 @@
 import javax.swing.JOptionPane;    
+import java.io.* ;
 
 public class TOPPUPIL 
 {
     // array of PUPIL objects
     private PUPIL pupilList [] ;
-    
     // number of pupils to be calculated after reading file
     int noOfPupils ; 
+    FILEREADCSV examDataFile ;
+    
     public TOPPUPIL () 
     {
-       //
+       examDataFile = new FILEREADCSV () ;
     }
 
     public void processPUPIL(String dataItems)
