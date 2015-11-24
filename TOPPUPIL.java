1754508d470a55ef67287c0c2c527590bfebdad9
Guest User <Guest@stk-cs01-ws05.westlothian.sch.uk>
2015-11-24 10:34:27 +0000

    2.2

--- TOPPUPIL.java
+++ TOPPUPIL.java
@@ -8,27 +8,40 @@ public class TOPPUPIL
     // number of pupils to be calculated after reading file
     int noOfPupils ; 
     FILEREADCSV examDataFile ;
-    
+
     public TOPPUPIL () 
     {
-       examDataFile = new FILEREADCSV () ;
+        examDataFile = new FILEREADCSV () ;
     }
 
-    public void processPUPIL(String dataItems)
+    public void processPUPIL() throws IOException
     {
-       //
-       setUppupilList () ;
-       countTopMark () ;
+        //
+        setUppupilList () ;
+        countTopMark () ;
 
     }
 
-    private void setUppupilList()
+    private void setUppupilList() throws IOException
     {
-      // placeholder
+        // first user message
+
+        // First user message
+        System.out.println("ScotFit Club: Membership BMI update");
+        System.out.println("** Preparing to read data file.");
+
+        // read file, fetch data as String array containing the rows
+        String[] dataRows = examDataFile.readCSVtable();
+        // calculate the number of member rows, skip headings
+        noOfPupils= dataRows.length - 1;
+
+        // update user with number of rows with membership details
+        System.out.println("** " + noOfPupils + " rows read.\n\n");
     }
-    
+
     public void countTopMark ()
     {
-     // placeholder
+        // placeholder
     }
 }
+
