99987acc9e699fa251d85e5ff8351e0dc64a1219
Guest User <Guest@stk-cs01-ws05.westlothian.sch.uk>
2015-11-24 10:27:15 +0000

    2.0
    Start of sprint 2.
    created top pupil class

---
+++ TOPPUPIL.java
@@ -0,0 +1,32 @@
+import javax.swing.JOptionPane;    
+
+public class TOPPUPIL 
+{
+    // array of PUPIL objects
+    private PUPIL pupilList [] ;
+    
+    // number of pupils to be calculated after reading file
+    int noOfPupils ; 
+    public TOPPUPIL () 
+    {
+       //
+    }
+
+    public void processPUPIL(String dataItems)
+    {
+       //
+       setUppupilList () ;
+       countTopMark () ;
+
+    }
+
+    private void setUppupilList()
+    {
+      // placeholder
+    }
+    
+    public void countTopMark ()
+    {
+     // placeholder
+    }
+}
