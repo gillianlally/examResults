1cb15a906c1de7dd67d83a8d5410edbb995255b8
Guest User <Guest@stk-cs01-ws05.westlothian.sch.uk>
2015-11-26 09:42:59 +0000

    3.0

--- TOPPUPIL.java
+++ TOPPUPIL.java
@@ -26,15 +26,27 @@ public class TOPPUPIL
         //first user message
         System.out.println("Feeback of marks" ) ;
         System.out.println("** Preparing to read data file.") ;
-        
+
         // read file, fetch data as String array containing the rows
         String[] datarows = examDataFile.readCSVtable () ;
-        
+
         //calculate the number of member rows, skip headings
         noOfPUPILS = datarows.length - 1 ;
-        
+
         // update user with number of rows with membership details
-        System.out.println ("** " + noOfPUPILS +  " rows read . /n/n") ;
+        System.out.println ("** " + noOfPUPILS +  " rows read ." ) ;
+
+        // prepare array for pupils 
+        pupilList = new PUPIL[noOfPUPILS] ;
+
+        //create pupil objects and copy data from source
+        for (int i = 0 ; i < noOfPUPILS ; i++) {
+            pupilList[i] = new PUPIL () ;
+
+            //adjust to skip headings 
+            pupilList[i].readPupilDetails(datarows[ i +1]);
+        }
+
     }
 
     public void counttopMark () 
